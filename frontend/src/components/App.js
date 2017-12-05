564ee5f50cfba4bbfcfef47bf28c9e042657df7c
Tacal, Tudor D. (LNG-LON) <TACALTD@legal.regn.net>
2017-12-05 22:49:32 +0000

    feat: Create category view

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,37 +4,67 @@ import * as api from "../utils/readableApi";
 import getPostsAsync from "../actions/getPosts";
 import getCategoriesAsync from "../actions/getCategories";
 import { fetchPosts } from "../utils/readableApi";
-import { Route, Router, Link, history } from "react-router-dom";
-import sortBy from 'sort-by';
-
+import { Route, Router, Link, history, withRouter } from "react-router-dom";
+import sortBy from "sort-by";
 
 class App extends Component {
-    componentDidMount() {
-        this.props.getPosts()
-        this.props.getCategories();
-    }
+  componentDidMount() {
+    this.props.getPosts();
+    this.props.getCategories();
+  }
   render() {
-      let posts = this.props.posts;
+    let posts = this.props.posts;
+    let categories = this.props.categories;
     return (
       <div className="App">
-      <div className="categories">Categories:
-        {this.props.categories.map(category => (
-                <Link key={category.name} to={`/${category.path}`}>{category.name}</Link>
+
+        <Route exact path="/" render={(history) => (
+            <div>
+              <div>
+                Categories:
+                {this.props.categories.map(category => (
+                  <div key={category.path} className="categories">
+                        <Link key={category.name} to={`/${category.path}`}>
+                        {category.name}
+                        </Link>
+                  </div>
+                ))}
+              </div>
+              <div className="posts">
+                <ul>
+                  Posts:
+                  {posts.map(post => (
+                    <li key={post.id}>
+                      <h2>{post.title}</h2>
+                      <h4>{post.body}</h4>
+                      <p>Category: {post.category}</p>
+                      <p>Votes: {post.voteScore}</p>
+                      <p>Time: {new Date(post.timestamp).toGMTString()}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          )}/>
+
+        {categories.map(category => (
+            <Route key={category.name}
+            path={`/${category.path}`}
+            render={(history) => (
+              <div>
+                  {posts.filter(post => post.category === category.name).map(post => (
+                      <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <h4>{post.body}</h4>
+                        <p>Category: {post.category}</p>
+                        <p>Votes: {post.voteScore}</p>
+                        <p>Time: {new Date(post.timestamp).toGMTString()}</p>
+                      </div>
+                    ))}
+              </div>
+            )}
+          />
         ))}
-      </div>
-        <div className="posts">
-        <ul>Posts:
-            {posts.map(post => (
-                <li key={post.id} >
-                    <h2>{post.title}</h2>
-                    <h4>{post.body}</h4>
-                    <p>Category: {post.category}</p>
-                    <p>Votes: {post.voteScore}</p>
-                    <p>Time: {new Date(post.timestamp).toGMTString()}</p>
-                </li>
-            ))}
-          </ul>
-        </div>
       </div>
     );
   }
@@ -54,9 +84,9 @@ function mapDispatchToProps(dispatch) {
       dispatch(getPostsAsync());
     },
     getCategories: () => {
-        dispatch(getCategoriesAsync());        
+      dispatch(getCategoriesAsync());
     }
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
