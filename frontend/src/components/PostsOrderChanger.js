ce907ca7a33130cd29ad7b9c9c2edee7099a6a1d
Tacal, Tudor D. (LNG-LON) <tudortacal@gmail.com>
2018-01-06 14:20:41 +0000

    style: Add style to PostOrderChanger component

--- frontend/src/components/PostsOrderChanger.js
+++ frontend/src/components/PostsOrderChanger.js
@@ -3,17 +3,17 @@ import React, { Component } from "react";
 function PostsOrderChanger(props) {
   return (
     <div className="postsOrderChanger">
-      <select
+      <select className="sortBox"
         defaultValue={props.postOrderBy || "none"}
         onChange={event => {
           props.onChangeHandler(event.target.value);
         }}
       >
-        <option value="none" disabled>
+        <option className="sortBoxOption" value="none" disabled>
           Sort by...
         </option>
-        <option value="voteScore">Vote Score</option>
-        <option value="timestamp">Timestamp</option>
+        <option className="sortBoxOption" value="voteScore">Vote Score</option>
+        <option className="sortBoxOption" value="timestamp">Timestamp</option>
       </select>
     </div>
   );
